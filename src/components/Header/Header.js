aecaca2ecc170764f15e18332d5d4b67f36ae5fb
Your Name <pronaybaura9@gmail.com>
2021-07-11 19:52:07 +0600

    responsive

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,30 +1,37 @@
 import React from 'react';
 import logo from '../../images/logo.png';
-import Auth, { UserAuth } from '../Login/UserAuth';
+import { UserAuth } from '../Login/UserAuth';
 import './Header.css';
+import { Nav, Navbar } from 'react-bootstrap';
 
 const Header = () => {
-    const userAuth = UserAuth();
-    console.log(userAuth);
-    return (
-        <div className="header-logo">
-            <img src={logo} alt="" />
-            <nav>
-                <a href="/shop" >Shop</a>
-                <a href="/review" >Order Review</a>
-                <a href="/inventory" >Manage Inventory</a>
-                {
-                    userAuth.user && <span style={{color:"yellow"}} >{userAuth.user.name}</span> 
-                }
-                {
-                    userAuth.user ?
-                    <a href="/login" >Sign Out</a> :
-                    <a href="/login" >Sign IN</a>
-                }
-            </nav>
-            
-        </div>
-    );
+  const userAuth = UserAuth();
+
+  return (
+    <div className="header-logo">
+      <img src={logo} alt="" />
+      <Navbar expand="lg"  >
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar/-nav">
+          <Nav className="m-auto">
+            <a href="/shop"> Shop </a>
+            <a href="/review"> Order Review </a>
+            <a href="/inventory"> Manage Inventory </a>
+            {userAuth.user && (
+              <span className="mr-3" style={{ color: 'yellow' }}> {userAuth.user.name} </span>
+            )}
+            {userAuth.user ? (
+              <a href="/login" > Sign Out </a>
+            ) : (
+              <a href="/login"> Sign IN </a>
+            )}
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+
+
+    </div>
+  );
 };
 
 export default Header;
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,30 +1,28 @@
 import React from 'react';
 import Auth from './UserAuth';
 
- 
 const Login = () => {
     const auth = Auth();
 
     const handleSignIN = () => {
-        auth.isSignWithGoogle().then(res => {
-       window.location.pathname = '/review';
-  })
-    }
+        auth.isSignWithGoogle().then((res) => {
+            window.location.pathname = '/review';
+        });
+    };
     const handleSignOut = () => {
-        auth.isSignWithGoogleOut().then(res =>{ 
-        window.location.pathname = '/shop';
-        })
-    }
+        auth.isSignWithGoogleOut().then((res) => {
+            window.location.pathname = '/shop';
+        });
+    };
 
- 
     return (
         <div>
-        <h1>Please login user</h1> 
-        {
-            auth.user ?
-            <button onClick={handleSignOut}>Sign Out</button> :
-            <button onClick={handleSignIN}>Sign IN</button>
-        }
+            <h1>Please login user</h1>
+            {auth.user ? (
+                <button onClick={handleSignOut}>Sign Out</button>
+            ) : (
+                <button onClick={handleSignIN}>Sign IN</button>
+            )}
         </div>
     );
 };
